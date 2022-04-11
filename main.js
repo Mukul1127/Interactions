878671b1eab1b289630553ca0ec4aa45b18457a4
Mukul1127 <71291707+Mukul1127@users.noreply.github.com>
2022-04-10 20:32:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// Setup
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+document.body.appendChild( renderer.domElement );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth - window.innerWidth / 16, window.innerHeight - window.innerWidth / 16);
+camera.position.setZ(30);
+camera.position.setX(-3);
+renderer.render( scene, camera );
+
+// Torus
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347, wireframe: true });
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+// Lights
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(1000));
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(2000).fill().forEach(addStar);
+
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+  controls.update();
+  renderer.render( scene, camera );
+}
+animate();
+
+// Resize Fix
+window.addEventListener( 'resize', onWindowResize );
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}

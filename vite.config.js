4ed908615e31ff776d89cc508d706a64038381c2
Mukul1127 <71291707+Mukul1127@users.noreply.github.com>
2022-04-11 18:07:08 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        keys: resolve(__dirname, 'key.html'),
-        visualizer: resolve(__dirname, '3d.html')
+        keys: resolve(__dirname, 'Keys.html'),
+        visualizer: resolve(__dirname, '3D.html')
       }
     }
   }
